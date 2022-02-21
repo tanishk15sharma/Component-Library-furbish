5082cc1cfd134c783bb35ea4d4f3277390d4b3a7
tanishk15sharma <tanishk1582002@gmail.com>
2022-02-21 23:16:09 +0530

    feat: refactor code snipptes

--- scripts/docs_nav.js
+++ scripts/docs_nav.js
@@ -1,8 +1,6 @@
 const toggleSlideBtn = document.querySelector(".menu-logo");
 const sideBar = document.querySelector(".docx-sidebar");
 
-console.log(toggleSlideBtn);
-
 toggleSlideBtn.addEventListener("click", () => {
   sideBar.classList.toggle("show");
 });
