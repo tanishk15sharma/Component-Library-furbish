bcf0444d56aa05b9c9129d0ccbc70c781a77dc1c
tanishk15sharma <tanishk1582002@gmail.com>
2022-02-21 22:01:29 +0530

    refactored code snippets

--- scripts/docs_nav.js
+++ scripts/docs_nav.js
@@ -1,5 +1,6 @@
 const toggleSlideBtn = document.querySelector(".menu-logo");
 const sideBar = document.querySelector(".docx-sidebar");
+
 console.log(toggleSlideBtn);
 
 toggleSlideBtn.addEventListener("click", () => {
